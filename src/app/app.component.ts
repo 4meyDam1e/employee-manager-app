a94aecc350d400d511ae8bec0953bddcb4fcc360
Destructa321 <amey.damle@mail.utoronto.ca>
2022-04-28 00:02:28 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.getAllEmployees();
+  }
+
+  public getAllEmployees(): void {
+    this.employeeService.getAllEmployees().subscribe({
+      next: (response: Employee[]) => {
+        this.employees = response;
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    });
+  }
+}
